059a7483090b9956f7d7d689739e46ac59e8cff6
Andrew Smirnov <sandrew2003@gmail.com>
2021-10-31 18:02:40 +0300

    add Sum project

---
+++ Sum/src/Sum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String ... args) {
+        var sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
